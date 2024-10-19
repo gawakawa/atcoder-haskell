e10f017d2ba1eda5b024af7f919ce0dae6f20f43
gawakawa <hirakuhasegawa@gmail.com>
2024-10-19 20:49:03 +0900

    ✨ solved

--- contests/abc375/a/Main.hs
+++ contests/abc375/a/Main.hs
@@ -4,6 +4,14 @@ module Main where
 
 import qualified Data.ByteString.Char8 as BS
 
+solve :: [ Char ] -> Int
+solve (first : second : third : rest)
+    | first == '#' && second == '.' && third == '#' = 1 + solve (third : rest)
+    | otherwise = solve $ second : third : rest
+solve _ = 0
+
 main :: IO ()
 main = do
-    undefined
+    _ <- readLn :: IO Int
+    s <- BS.unpack <$> BS.getLine
+    print $ solve s
