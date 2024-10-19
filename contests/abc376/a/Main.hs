74ea9eadd21f4dcc631393c76df3e448cbe2c9ed
gawakawa <hirakuhasegawa@gmail.com>
2024-10-19 22:18:32 +0900

    🎉 abc376

---
+++ contests/abc376/a/Main.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE StrictData #-}
+
+module Main where
+
+import qualified Data.ByteString.Char8 as BS
+
+main :: IO ()
+main = do
+    undefined
+
---
+++ contests/abc376/d/Main.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE StrictData #-}
+
+module Main where
+
+import qualified Data.ByteString.Char8 as BS
+
+main :: IO ()
+main = do
+    undefined
