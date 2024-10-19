596cf744d78224c7601ba5d6ecdd2d0560ffeb56
gawakawa <hirakuhasegawa@gmail.com>
2024-10-19 19:04:14 +0900

    ✨ Solved

--- contests/abs/abc081b/Main.hs
+++ contests/abs/abc081b/Main.hs
@@ -1,36 +1,15 @@
-#!/usr/bin/env stack
-{- stack script --resolver lts-21.6 -}
-
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE NumDecimals #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StrictData #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# OPTIONS_GHC -Wno-unused-imports -Wno-unused-top-binds -Wno-missing-signatures -Wno-unused-matches -funbox-small-strict-fields #-}
 
 module Main where
 
-import Control.Applicative
-import Control.Monad
-import Data.Array qualified as A
-import Data.Array.Unboxed qualified as AU
-import Data.ByteString.Char8 qualified as BS
-import Data.Char
-import Data.Function
-import Data.Int
-import Data.List qualified as L
-import Data.Maybe
-import Data.Proxy
-import Data.Reflection
-import Data.Vector qualified as V
-import Data.Vector.Generic qualified as VG
-import Data.Vector.Unboxed qualified as VU
-import Data.Vector.Unboxing qualified as Unboxing
-import GHC.TypeLits
-
-
 main :: IO ()
 main = do
-    undefined
+    _ <- readLn :: IO Int
+    as <- map read . words <$> getLine :: IO [ Int ]
+    print $ solve as
+
+solve :: [ Int ] -> Int 
+solve as = 
+    if all even as 
+    then 1 + solve (map (`div` 2) as) 
+    else 0
--- contests/abs/abc087b/Main.hs
+++ contests/abs/abc087b/Main.hs
@@ -1,36 +1,15 @@
-#!/usr/bin/env stack
-{- stack script --resolver lts-21.6 -}
-
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE NumDecimals #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StrictData #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# OPTIONS_GHC -Wno-unused-imports -Wno-unused-top-binds -Wno-missing-signatures -Wno-unused-matches -funbox-small-strict-fields #-}
 
 module Main where
 
-import Control.Applicative
-import Control.Monad
-import Data.Array qualified as A
-import Data.Array.Unboxed qualified as AU
-import Data.ByteString.Char8 qualified as BS
-import Data.Char
-import Data.Function
-import Data.Int
-import Data.List qualified as L
-import Data.Maybe
-import Data.Proxy
-import Data.Reflection
-import Data.Vector qualified as V
-import Data.Vector.Generic qualified as VG
-import Data.Vector.Unboxed qualified as VU
-import Data.Vector.Unboxing qualified as Unboxing
-import GHC.TypeLits
-
-
 main :: IO ()
 main = do
-    undefined
+    [ a, b, c, x ] <- map read . lines <$> getContents :: IO [ Int ]
+    print $ solve a b c x
+
+solve :: Int -> Int -> Int -> Int -> Int
+solve a b c x = 
+    length $ filter (== x) [ 500 * a' + 100 * b' + 50 * c' 
+                           | a' <- [ 0 .. a ]
+                           , b' <- [ 0 .. b ]
+                           , c' <- [ 0 .. c ]]
