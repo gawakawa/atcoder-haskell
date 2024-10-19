50b5448dc5e59b8e8b8df54212f72d96d74ae14d
gawakawa <hirakuhasegawa@gmail.com>
2024-10-19 22:19:21 +0900

    ✨ Solved

--- contests/abc376/a/Main.hs
+++ contests/abc376/a/Main.hs
@@ -4,7 +4,23 @@ module Main where
 
 import qualified Data.ByteString.Char8 as BS
 
+solve :: Int -> [ Int ] -> Int
+solve _ [] = 0
+solve c (t : ts) = 1 + solve' ts t
+    where
+        solve' :: [ Int ] -> Int -> Int
+        solve' [] _ = 0
+        solve' [ putButtonTime ] getBefore = 
+            if putButtonTime - getBefore < c
+                then 0
+                else 1
+        solve' (putButtonTime : putButtonTimes) getBefore = 
+            if putButtonTime - getBefore < c
+                then solve' putButtonTimes getBefore
+                else 1 + solve' putButtonTimes putButtonTime
+
 main :: IO ()
 main = do
-    undefined
-
+    [ _, c ] <- map read . words <$> getLine :: IO [ Int ]
+    ts <- map read . words <$> getLine :: IO [ Int ]
+    print $ solve c ts
