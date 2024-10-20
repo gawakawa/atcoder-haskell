0c50ccf4dc49a49cd4926384b6e4bed3401e59a9
gawakawa <hirakuhasegawa@gmail.com>
2024-10-20 17:33:33 +0900

    ✅ Solved

--- contests/abs/abc088b/Main.hs
+++ contests/abs/abc088b/Main.hs
@@ -1,36 +1,17 @@
-#!/usr/bin/env stack
-{- stack script --resolver lts-21.6 -}
-
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE NumDecimals #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StrictData #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# OPTIONS_GHC -Wno-unused-imports -Wno-unused-top-binds -Wno-missing-signatures -Wno-unused-matches -funbox-small-strict-fields #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
 
 module Main where
 
-import Control.Applicative
-import Control.Monad
-import Data.Array qualified as A
-import Data.Array.Unboxed qualified as AU
-import Data.ByteString.Char8 qualified as BS
-import Data.Char
-import Data.Function
-import Data.Int
-import Data.List qualified as L
-import Data.Maybe
-import Data.Proxy
-import Data.Reflection
-import Data.Vector qualified as V
-import Data.Vector.Generic qualified as VG
-import Data.Vector.Unboxed qualified as VU
-import Data.Vector.Unboxing qualified as Unboxing
-import GHC.TypeLits
+import Data.List
+import Data.Ord
 
+solve :: [ Int ] -> Int
+solve = 
+    sum . zipWith (*) (cycle [ 1, -1 ]) . sortBy (comparing Down)
 
 main :: IO ()
 main = do
-    undefined
+    _ <- readLn :: IO Int
+    as <- map read . words <$> getLine :: IO [ Int ]
+    print $ solve as
