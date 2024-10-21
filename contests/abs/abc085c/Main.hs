8a24ace2bf1ca4172a45fbf1f1ef7a5f45199835
gawakawa <hirakuhasegawa@gmail.com>
2024-10-21 21:51:59 +0900

    ✨ Solved

--- contests/abs/abc085c/Main.hs
+++ contests/abs/abc085c/Main.hs
@@ -1,36 +1,17 @@
-#!/usr/bin/env stack
-{- stack script --resolver lts-21.6 -}
-
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE NumDecimals #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StrictData #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# OPTIONS_GHC -Wno-unused-imports -Wno-unused-top-binds -Wno-missing-signatures -Wno-unused-matches -funbox-small-strict-fields #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
 
 module Main where
 
-import Control.Applicative
-import Control.Monad
-import Data.Array qualified as A
-import Data.Array.Unboxed qualified as AU
-import Data.ByteString.Char8 qualified as BS
-import Data.Char
-import Data.Function
-import Data.Int
-import Data.List qualified as L
-import Data.Maybe
-import Data.Proxy
-import Data.Reflection
-import Data.Vector qualified as V
-import Data.Vector.Generic qualified as VG
-import Data.Vector.Unboxed qualified as VU
-import Data.Vector.Unboxing qualified as Unboxing
-import GHC.TypeLits
-
+solve :: Int -> Int -> [ Int ]
+solve n y = head $
+    [ [ a, b, c ] | a <- [ 0 .. n ]
+                  , b <- [ 0 .. n - a ]
+                  , let c = n - a - b
+                  , 10000 * a + 5000 * b + 1000 * c == y
+    ] ++ [ [ -1, -1, -1 ] ]
 
 main :: IO ()
 main = do
-    undefined
+    [ n, y ] <- map read . words <$> getLine :: IO [ Int ]
+    putStrLn $ unwords $ show <$> solve n y
