45cfac75905b7665bbffc8374d2ca71399f403ec
gawakawa <hirakuhasegawa@gmail.com>
2024-10-21 20:43:51 +0900

    ✨ Solved

--- contests/abs/abc083b/Main.hs
+++ contests/abs/abc083b/Main.hs
@@ -1,36 +1,17 @@
-#!/usr/bin/env stack
-{- stack script --resolver lts-21.6 -}
-
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE NumDecimals #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StrictData #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# OPTIONS_GHC -Wno-unused-imports -Wno-unused-top-binds -Wno-missing-signatures -Wno-unused-matches -funbox-small-strict-fields #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
 
 module Main where
 
-import Control.Applicative
-import Control.Monad
-import Data.Array qualified as A
-import Data.Array.Unboxed qualified as AU
-import Data.ByteString.Char8 qualified as BS
-import Data.Char
-import Data.Function
-import Data.Int
-import Data.List qualified as L
-import Data.Maybe
-import Data.Proxy
-import Data.Reflection
-import Data.Vector qualified as V
-import Data.Vector.Generic qualified as VG
-import Data.Vector.Unboxed qualified as VU
-import Data.Vector.Unboxing qualified as Unboxing
-import GHC.TypeLits
+solve :: Int -> Int -> Int -> Int
+solve n a b = 
+    sum [ x | x <- [ 1 .. n ], a <= sumDigits x, sumDigits x <= b ]
 
+sumDigits :: Int -> Int
+sumDigits 0 = 0
+sumDigits n = n `mod` 10 + sumDigits (n `div` 10)
 
 main :: IO ()
 main = do
-    undefined
+    [ n, a, b ] <- map read . words <$> getLine :: IO [ Int ]
+    print $ solve n a b
