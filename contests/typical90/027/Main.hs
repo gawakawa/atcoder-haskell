66900857030655048852ff845a2f1ba5c6000b1f
gawakawa <hirakuhasegawa@gmail.com>
2024-10-23 21:57:54 +0900

    ⚡️ Upsolved

--- contests/typical90/027/Main.hs
+++ contests/typical90/027/Main.hs
@@ -4,7 +4,21 @@
 module Main where
 
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.HashSet as HS
+import qualified Data.Vector.Unboxed as VU
+
+solve :: [ BS.ByteString ] -> [ Int ]
+solve = solve' 1 HS.empty
+    where
+        solve' :: Int -> HS.HashSet BS.ByteString -> [ BS.ByteString ] -> [ Int ]
+        solve' _ _          []       = []
+        solve' i registered (u : us) = 
+            if HS.member u registered
+                then     solve' (i + 1) registered  us
+                else i : solve' (i + 1) (HS.insert u registered) us
 
 main :: IO ()
 main = do
-    undefined
+    _  <- readLn @Int
+    ss <- BS.words <$> BS.getContents
+    putStr $ unlines $ show <$> solve ss
