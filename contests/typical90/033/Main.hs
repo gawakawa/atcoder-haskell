f5df62c20fde8b4974d10ea90fa89f636cae9810
gawakawa <hirakuhasegawa@gmail.com>
2024-10-24 06:56:25 +0900

    ⚡️ Upsolved

--- contests/typical90/033/Main.hs
+++ contests/typical90/033/Main.hs
@@ -3,8 +3,20 @@
 
 module Main where
 
+import Control.Monad (ap)
+import Data.Bool (bool)
+import Data.Function (on)
+import Data.Maybe (fromJust)
+
 import qualified Data.ByteString.Char8 as BS
+
+solve :: Int -> Int -> Int
+solve h w =
+    if ((||) `on` (== 1)) h w
+        then h * w
+        else ((*) `on` \n -> (n + 1) `div` 2) h w
 
 main :: IO ()
 main = do
-    undefined
+    [ h, w ] <- map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
+    print $ solve h w
