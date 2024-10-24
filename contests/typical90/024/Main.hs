a1dd485ac220516f936c77cfda982da8829fb1ab
gawakawa <hirakuhasegawa@gmail.com>
2024-10-25 00:17:27 +0900

    ✨ Solved

--- contests/typical90/024/Main.hs
+++ contests/typical90/024/Main.hs
@@ -3,8 +3,23 @@
 
 module Main where
 
+import Control.Applicative (liftA2)
+import Data.Maybe (fromJust)
+
 import qualified Data.ByteString.Char8 as BS
+
+solve :: Int -> [ Int ] -> [ Int ] -> Bool
+solve k = ((plus2n . (k -)) .) . sumDiff
+
+plus2n :: Int -> Bool
+plus2n = liftA2 (&&) (>= 0) even
+
+sumDiff :: [ Int ] -> [ Int ] -> Int
+sumDiff = (sum .) . zipWith ((abs .) . subtract)
 
 main :: IO ()
 main = do
-    undefined
+    [ _, k ] <- map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
+    as       <- map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
+    bs       <- map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
+    putStrLn $ if solve k as bs then "Yes" else "No"
