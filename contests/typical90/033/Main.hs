b182b2f5209e7ab4a2c0d3d33b38797323bbfcdc
gawakawa <hirakuhasegawa@gmail.com>
2024-10-24 20:30:01 +0900

    ⚡️ Upsolved

--- contests/typical90/033/Main.hs
+++ contests/typical90/033/Main.hs
@@ -4,7 +4,7 @@
 module Main where
 
 import Control.Monad (ap)
-import Data.Bool (bool)
+import Data.Bool.HT (if')
 import Data.Function (on)
 import Data.Maybe (fromJust)
 
@@ -16,7 +16,14 @@ solve h w =
         then h * w
         else ((*) `on` \n -> (n + 1) `div` 2) h w
 
+solve' :: Int -> Int -> Int
+solve' = ap . ap (ap . (if' .) . cnd) thn <*> els
+    where
+        cnd = (||) `on` (== 1)
+        thn = (*)
+        els = (*) `on` \n -> (n + 1) `div` 2
+
 main :: IO ()
 main = do
     [ h, w ] <- map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
-    print $ solve h w
+    print $ solve' h w
