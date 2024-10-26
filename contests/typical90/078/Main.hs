7667f54e4288c5bc517602a44b83366f645684e4
gawakawa <hirakuhasegawa@gmail.com>
2024-10-27 00:16:05 +0900

    ⚡️ Upsolved

--- contests/typical90/078/Main.hs
+++ contests/typical90/078/Main.hs
@@ -3,8 +3,23 @@
 
 module Main where
 
+import Control.Monad (replicateM)
+import Data.Array (Array, (!))
+import Data.Graph (Graph, Vertex, buildG, vertices)
+import Data.Maybe (fromJust)
+
 import qualified Data.ByteString.Char8 as BS
+
+solve :: Graph -> Int
+solve g = length [ () | v <- vertices g, hasOneSmallerAdjacents v (g ! v) ]
+
+hasOneSmallerAdjacents :: Vertex -> [ Vertex ] -> Bool
+hasOneSmallerAdjacents v = (== 1) . length . filter (< v)
 
 main :: IO ()
 main = do
-    undefined
+    [ n, m ] <- map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
+    edges    <- replicateM m $ do
+        [ a, b ] <- map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
+        pure (a, b)
+    print $ solve $ buildG (1, n) $ concatMap (\(u, v) -> [ (u, v), (v, u) ]) edges
