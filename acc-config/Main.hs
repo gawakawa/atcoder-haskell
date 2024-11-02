31f7393fbb699124c0edb8c9a0eaa392bb40d78d
gawakawa <hirakuhasegawa@gmail.com>
2024-11-02 21:38:34 +0900

    🎨 Add functions for my library

--- acc-config/Main.hs
+++ acc-config/Main.hs
@@ -13,6 +13,43 @@ import Data.Sequence qualified as Seq
 import Data.Set qualified as S
 import Data.Vector.Unboxed qualified as VU
 
+import Control.Applicative (liftA3)
+import Control.Arrow ((>>>))
+import Control.Monad (replicateM)
+import Data.Array.Unboxed (UArray, (!), bounds, listArray, range)
+import Data.Bool.HT (if')
+import Data.Char (digitToInt, intToDigit)
+import Data.Functor ((<&>))
+import Data.List (foldl')
+import Data.Maybe (fromJust)
+import Data.Tuple.Extra (both)
+
 main :: IO ()
 main = do
     undefined
+
+-- my lib
+ints :: IO [ Int ]
+ints = BS.getLine <&> (BS.words >>> map (BS.readInt >>> fromJust >>> fst))
+
+intMat :: Int -> Int -> IO (UArray (Int, Int) Int)
+intMat h w = replicateM h ints <&> (concat >>> listArray @UArray ((1, 1), (h, w)))
+
+showIntMat :: UArray (Int, Int) Int -> String
+showIntMat mat =
+    unlines
+        [ unwords
+            [ show $ mat ! (r, c)
+            | c <- range $ both snd $ bounds mat
+            ]
+        | r <- range $ both fst $ bounds mat
+        ]
+
+fromBase :: Int -> String -> Int
+fromBase n = foldl' (\acc d -> acc * n + digitToInt d) 0
+
+toBase :: Int -> Int -> String
+toBase n = liftA3 if' (== 0) 
+    (const "0") 
+    $ iterate (`div` n) >>> takeWhile (> 0) 
+      >>> foldl' (flip $ (`mod` n) >>> intToDigit >>> (:)) ""
