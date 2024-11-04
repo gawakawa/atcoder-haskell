5063a1bb691f563a493580958016c35c03bd7af4
gawakawa <hirakuhasegawa@gmail.com>
2024-11-05 07:19:57 +0900

    ✨ Solved

--- contests/typical90/022/Main.hs
+++ contests/typical90/022/Main.hs
@@ -3,8 +3,60 @@
 
 module Main where
 
-import qualified Data.ByteString.Char8 as BS
+import Data.ByteString.Char8 qualified as BS
+import Data.IntMap.Strict qualified as IM
+import Data.IntSet qualified as IS
+import Data.HashMap.Strict qualified as HM
+import Data.HashSet qualified as HS
+import Data.Map.Strict qualified as M
+import Data.Sequence qualified as Seq
+import Data.Set qualified as S
+import Data.Vector.Unboxed qualified as VU
+
+import Control.Applicative (liftA3)
+import Control.Arrow ((>>>))
+import Control.Monad (replicateM)
+import Data.Array.Unboxed (UArray, (!), bounds, listArray, range)
+import Data.Bool.HT (if')
+import Data.Char (digitToInt, intToDigit)
+import Data.Functor ((<&>))
+import Data.List (foldl')
+import Data.Maybe (fromJust)
+import Data.Tuple.Extra (both)
+
+solve :: Int -> Int -> Int -> Int
+solve a b c =
+    (a `div` g - 1) + (b `div` g - 1) + (c `div` g - 1)
+    where
+        g = gcd a $ gcd b c
 
 main :: IO ()
 main = do
-    undefined
+    [ a, b, c ] <- ints
+    print $ solve a b c
+
+-- my lib
+ints :: IO [ Int ]
+ints = BS.getLine <&> (BS.words >>> map (BS.readInt >>> fromJust >>> fst))
+
+intMat :: Int -> Int -> IO (UArray (Int, Int) Int)
+intMat h w = replicateM h ints <&> (concat >>> listArray @UArray ((1, 1), (h, w)))
+
+showIntMat :: UArray (Int, Int) Int -> String
+showIntMat mat =
+    unlines
+        [ unwords
+            [ show $ mat ! (r, c)
+            | c <- range $ both snd $ bounds mat
+            ]
+        | r <- range $ both fst $ bounds mat
+        ]
+
+fromBase :: Int -> String -> Int
+fromBase n = foldl' (\acc d -> acc * n + digitToInt d) 0
+
+toBase :: Int -> Int -> String
+toBase n = liftA3 if' (== 0) 
+    (const "0") 
+    $ iterate (`div` n) >>> takeWhile (> 0) 
+      >>> foldl' (flip $ (`mod` n) >>> intToDigit >>> (:)) ""
