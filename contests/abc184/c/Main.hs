0f44f17efa8a651b56e3a8ea0f85c65a7a354649
gawakawa <hirakuhasegawa@gmail.com>
2024-12-07 18:30:52 +0900

    ✨ Solved

--- contests/abc184/c/Main.hs
+++ contests/abc184/c/Main.hs
@@ -15,7 +15,7 @@ import Data.Vector.Unboxed qualified as VU
 
 import Control.Applicative (liftA3)
 import Control.Arrow ((>>>))
-import Control.Monad (replicateM)
+import Control.Monad (guard, replicateM)
 import Data.Array.Unboxed (UArray, (!), bounds, listArray, range)
 import Data.Bool.HT (if')
 import Data.Char (digitToInt, intToDigit)
@@ -24,9 +24,25 @@ import Data.List (foldl')
 import Data.Maybe (fromJust)
 import Data.Tuple.Extra (both)
 
+solve :: (Int, Int) -> Int
+solve (0, 0) = 0
+solve (c, r) 
+    | abs c + abs r <= 3   = 1
+    | abs c - abs r == 0   = 1
+    | even $ abs c + abs r = 2
+    | otherwise            = succ $ minimum $ do
+        c' <- [ -3 .. 3 ]
+        r' <- [ -3 .. 3 ]
+        guard $ abs c' + abs r' <= 3
+        guard $ abs c' - abs r' /= 0
+        guard $ odd $ abs c' + abs r'
+        pure  $ solve (c - c', r - r')
+
 main :: IO ()
 main = do
-    undefined
+    [ r1, c1 ] <- ints
+    [ r2, c2 ] <- ints
+    print $ solve (r2 - r1, c2 - c1)
 
 -- my lib
 ints :: IO [ Int ]
