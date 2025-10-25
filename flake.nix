f00568471b81415c54fde0de7ccd2073a175a6ad
gawakawa <mashurakiryu@icloud.com>
2025-10-25 22:46:48 +0900

    🔧 Replace flake-utils with flake-parts

--- flake.nix
+++ flake.nix
@@ -3,102 +3,112 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+    inputs@{
+      flake-parts,
+      nixpkgs,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-        atcoder-cli = pkgs.buildNpmPackage {
-          pname = "atcoder-cli";
-          version = "2.2.0";
+      perSystem =
+        { system, pkgs, ... }:
+        let
+          atcoder-cli = pkgs.buildNpmPackage {
+            pname = "atcoder-cli";
+            version = "2.2.0";
 
-          src = pkgs.fetchFromGitHub {
-            owner = "Tatamo";
-            repo = "atcoder-cli";
-            rev = "v2.2.0";
-            hash = "sha256-7pbCTgWt+khKVyMV03HanvuOX2uAC0PL9OLmqly7IWE=";
-          };
-
-          npmDepsHash = "sha256-ufG7Fq5D2SOzUp8KYRYUB5tYJYoADuhK+2zDfG0a3ks=";
-
-          nativeBuildInputs = [ pkgs.nodejs_20 ];
-
-          NODE_OPTIONS = "--openssl-legacy-provider";
-
-          dontNpmBuild = true;
-        };
-
-        oj-verify =
-          with pkgs.python3Packages;
-          pkgs.python3Packages.buildPythonApplication {
-            name = "verification-helper";
-            version = "5.6.0";
-            pyproject = true;
             src = pkgs.fetchFromGitHub {
-              owner = "online-judge-tools";
-              repo = "verification-helper";
-              rev = "adbff121b1f96de5f34e9f1483eb47d661c54075";
-              fetchSubmodules = false;
-              sha256 = "sha256-f7Ge8kLRQv9uxdNGtgNsypGVY0XAnKPCg8HYQ5nT6mI=";
+              owner = "Tatamo";
+              repo = "atcoder-cli";
+              rev = "v2.2.0";
+              hash = "sha256-7pbCTgWt+khKVyMV03HanvuOX2uAC0PL9OLmqly7IWE=";
             };
-            build-system = [ setuptools ];
-            dependencies = [
-              colorlog
-              importlab
-              online-judge-tools
-              pyyaml
-              setuptools
-              toml
-            ];
-            propagatedBuildInputs = [ setuptools ];
+
+            npmDepsHash = "sha256-ufG7Fq5D2SOzUp8KYRYUB5tYJYoADuhK+2zDfG0a3ks=";
+
+            nativeBuildInputs = [ pkgs.nodejs_20 ];
+
+            NODE_OPTIONS = "--openssl-legacy-provider";
+
+            dontNpmBuild = true;
           };
-      in
-      {
-        devShells.default =
-          with pkgs;
-          mkShell {
-            buildInputs = [
-              cabal-install
-              llvmPackages.bintools
-              pkg-config
-              stack
-            ];
 
-            packages = [
-              atcoder-cli
-              oj-verify
-              online-judge-tools
+          oj-verify =
+            with pkgs.python3Packages;
+            pkgs.python3Packages.buildPythonApplication {
+              name = "verification-helper";
+              version = "5.6.0";
+              pyproject = true;
+              src = pkgs.fetchFromGitHub {
+                owner = "online-judge-tools";
+                repo = "verification-helper";
+                rev = "adbff121b1f96de5f34e9f1483eb47d661c54075";
+                fetchSubmodules = false;
+                sha256 = "sha256-f7Ge8kLRQv9uxdNGtgNsypGVY0XAnKPCg8HYQ5nT6mI=";
+              };
+              build-system = [ setuptools ];
+              dependencies = [
+                colorlog
+                importlab
+                online-judge-tools
+                pyyaml
+                setuptools
+                toml
+              ];
+              propagatedBuildInputs = [ setuptools ];
+            };
+        in
+        {
+          devShells.default =
+            with pkgs;
+            mkShell {
+              buildInputs = [
+                cabal-install
+                llvmPackages.bintools
+                pkg-config
+                stack
+              ];
 
-              haskell.compiler.ghc984
-              haskellPackages.cabal-fmt
-              haskellPackages.doctest
-              haskellPackages.fourmolu
-              haskellPackages.ghcid
-              haskellPackages.ghcide
-              haskellPackages.haskell-language-server
-              haskellPackages.hoogle
-              haskellPackages.implicit-hie
-              hlint
-            ];
+              packages = [
+                atcoder-cli
+                oj-verify
+                online-judge-tools
 
-            shellHook = ''
-              acc config oj-path $(which oj)
-              export ROOT="$PWD"
+                haskell.compiler.ghc984
+                haskellPackages.cabal-fmt
+                haskellPackages.doctest
+                haskellPackages.fourmolu
+                haskellPackages.ghcid
+                haskellPackages.ghcide
+                haskellPackages.haskell-language-server
+                haskellPackages.hoogle
+                haskellPackages.implicit-hie
+                hlint
+              ];
 
-              alias t='$ROOT/test.sh $(basename $PWD)'
-              alias s='acc s'
-              alias ts='t && s'
+              shellHook = ''
+                acc config oj-path $(which oj)
+                export ROOT="$PWD"
 
-              new() {
-                  $ROOT/new.sh $1
-              }
-            '';
-          };
-      }
-    );
+                alias t='$ROOT/test.sh $(basename $PWD)'
+                alias s='acc s'
+                alias ts='t && s'
+
+                new() {
+                    $ROOT/new.sh $1
+                }
+              '';
+            };
+        };
+    };
 }
