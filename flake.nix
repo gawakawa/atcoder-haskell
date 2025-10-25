1c850804316d0dfa8746d38ade80eb36d8c8ad23
gawakawa <mashurakiryu@icloud.com>
2025-10-25 22:42:21 +0900

    ➖ Remove aclogin

--- flake.nix
+++ flake.nix
@@ -57,27 +57,6 @@
             ];
             propagatedBuildInputs = [ setuptools ];
           };
-
-        aclogin =
-          with pkgs.python3Packages;
-          pkgs.python3Packages.buildPythonApplication {
-            name = "aclogin";
-            version = "0.0.1";
-            format = "setuptools";
-            # doCheck = false;
-            src = pkgs.fetchFromGitHub {
-              owner = "key-moon";
-              repo = "aclogin";
-              rev = "e461311c0326578b16d1488be84261f4b24f6134";
-              fetchSubmodules = false;
-              sha256 = "sha256-kyU7KpFenFb7obwSrDp6dPfuE+36r0BGYerrJj3+EyA=";
-            };
-            dependencies = [
-              appdirs
-              requests
-            ];
-            propagatedBuildInputs = [ setuptools ];
-          };
       in
       {
         devShells.default =
@@ -91,7 +70,6 @@
             ];
 
             packages = [
-              aclogin
               atcoder-cli
               oj-verify
               online-judge-tools
